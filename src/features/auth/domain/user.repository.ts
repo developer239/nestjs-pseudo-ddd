33ef0449a73df0d8a86d86b2ff65307041982595
michaljarnot <m.jarnot@yahoo.com>
2023-03-04 00:24:23 +0100

    refactor: path absolute

--- src/features/auth/domain/user.repository.ts
+++ src/features/auth/domain/user.repository.ts
@@ -1,5 +1,5 @@
 import { UserModel } from 'src/features/auth/domain/user.model'
-import { CreateUserDTO } from '../presentation/dto/createUser.dto'
+import { CreateUserDTO } from 'src/features/auth/presentation/dto/createUser.dto'
 
 export abstract class UserRepository {
   public abstract findByUsername(username: string): Promise<UserModel>
